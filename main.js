cfc0a4b33f7c0b5d300d9651a32d5ecf4bdd7e12
cloudhuh <claudiu.huhulia@gmail.com>
2023-06-05 14:52:24 +0200

    ciclo_for

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 console.log('js-ok')
 
+const shop = ['pane','pasta','melanzane','latte','pollo','riso'];
+const shoplist = document.getElementById('spesa');
+
+
+let ElementList = '<ul>';
+
+
+for(i=0 ; i < shop.length; i++){
+
+    
+    ElementList += `<li> ${shop[i]} </li>`
+
+}
+
+ElementList += '</ul>';
+
+shoplist.innerHTML= ElementList;
